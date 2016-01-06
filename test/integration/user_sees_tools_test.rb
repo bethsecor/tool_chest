1682f6050b53e7977b52396bfd16261eaf98da2e
Beth Secor <elizabeth.a.secor@gmail.com>
2016-01-06 08:31:45 -0700

    Add sad path testing to user-tool association

--- test/integration/user_sees_tools_test.rb
+++ test/integration/user_sees_tools_test.rb
@@ -4,6 +4,7 @@ class UserSeesToolsTest < ActionDispatch::IntegrationTest
   test "logged in user sees list of their tools" do
     tool_1 = Tool.create(name: "Hammer", use: "thldshldts")
     tool_2 = Tool.create(name: "Wrench", use: "hrlahlrar")
+    tool_3 = Tool.create(name: "Screwdriver", use: "dlhslhhrhrhr")
 
     user = User.create(username: "Maddie", password: "password123")
     user.tools << tool_1
@@ -26,11 +27,12 @@ class UserSeesToolsTest < ActionDispatch::IntegrationTest
     click_button "Create Tool"
 
     visit user_path(user.id)
-    save_and_open_page
+    # save_and_open_page
     within('#tools') do
       assert page.has_content?("#{tool_1.name}")
       assert page.has_content?("#{tool_2.name}")
       assert page.has_content?("Pliers")
+      refute page.has_content?("Screwdriver")
     end
   end
 end
